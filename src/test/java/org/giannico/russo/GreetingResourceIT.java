f6b27010ff1d51adcf8652e880cf986ba6ad2561
Simone <simone.russo@itsincom.it>
2024-11-25 11:22:47 +0100

    Initial commit

---
+++ src/test/java/org/giannico/russo/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package org.giannico.russo;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
