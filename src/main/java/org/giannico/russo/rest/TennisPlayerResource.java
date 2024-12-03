4df2bd976643aa6bd82a4eba2744a03d80886a82
Fabio-Giannico <fabiogiannico3@gmail.com>
2024-12-03 17:16:04 +0100

    Implemented getAllTennisPlayersInAtpRanking

--- src/main/java/org/giannico/russo/rest/TennisPlayerResource.java
+++ src/main/java/org/giannico/russo/rest/TennisPlayerResource.java
@@ -1,4 +1,23 @@
 package org.giannico.russo.rest;
 
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import org.giannico.russo.service.TennisPlayerService;
+
+@Path("/tennis/player")
 public class TennisPlayerResource {
+    private final TennisPlayerService tennisPlayerService;
+
+    public TennisPlayerResource(TennisPlayerService tennisPlayerService) {
+        this.tennisPlayerService = tennisPlayerService;
+    }
+
+    @GET
+    @Path("/atp-all")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getAllTennisPlayersInAtpRanking(){
+        return tennisPlayerService.getAllTennisPlayersInAtpRanking();
+    }
 }
--- src/main/java/org/giannico/russo/service/TennisPlayerService.java
+++ src/main/java/org/giannico/russo/service/TennisPlayerService.java
@@ -1,4 +1,18 @@
 package org.giannico.russo.service;
 
+import jakarta.enterprise.context.ApplicationScoped;
+import org.giannico.russo.client.SofascoreClient;
+
+@ApplicationScoped
 public class TennisPlayerService {
+    private final SofascoreClient sofascoreClient;
+
+    public TennisPlayerService(SofascoreClient sofascoreClient) {
+        this.sofascoreClient = sofascoreClient;
+    }
+
+    public String getAllTennisPlayersInAtpRanking() {
+        String endpoint = "api/v1/rankings/type/5";
+        return sofascoreClient.fetchTennisData(endpoint);
+    }
 }
